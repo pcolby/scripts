bd390feef3f96e21fdb28c2b341a4802f1528766
Paul Colby <git@colby.id.au>
2013-10-06 17:01:18 +1100

    A small simplification - replacing a 'cat, grep, unset' sequence with a single sed instead.

--- cpu.sh
+++ cpu.sh
@@ -5,9 +5,9 @@ PREV_TOTAL=0
 PREV_IDLE=0
 
 while true; do
-  CPU=(`cat /proc/stat | grep '^cpu '`) # Get the total CPU statistics.
-  unset CPU[0]                          # Discard the "cpu" prefix.
-  IDLE=${CPU[4]}                        # Get the idle CPU time.
+  # Get the total CPU statistics, discarding the 'cpu ' prefix.
+  CPU=(`sed -n 's/^cpu\s//p' /proc/stat`)
+  IDLE=${CPU[3]} # Just the idle CPU time.
 
   # Calculate the total CPU time.
   TOTAL=0
