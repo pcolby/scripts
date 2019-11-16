d8437b4c09e23695533495d437544e7c2670fa3c
Paul Colby <git@colby.id.au>
2019-11-16 23:10:04 +1100

    Modernise some Bash syntax
    Also the same as fixing the following ShellCheck warnings:
    * SC2006: Use $(...) notation instead of legacy backticked `...`.
    * SC2219: Instead of 'let expr', prefer (( expr )) .

--- cpu.sh
+++ cpu.sh
@@ -6,19 +6,19 @@ PREV_IDLE=0
 
 while true; do
   # Get the total CPU statistics, discarding the 'cpu ' prefix.
-  CPU=(`sed -n 's/^cpu\s//p' /proc/stat`)
+  CPU=($(sed -n 's/^cpu\s//p' /proc/stat))
   IDLE=${CPU[3]} # Just the idle CPU time.
 
   # Calculate the total CPU time.
   TOTAL=0
   for VALUE in "${CPU[@]}"; do
-    let "TOTAL=$TOTAL+$VALUE"
+    TOTAL=$((TOTAL+VALUE))
   done
 
   # Calculate the CPU usage since we last checked.
-  let "DIFF_IDLE=$IDLE-$PREV_IDLE"
-  let "DIFF_TOTAL=$TOTAL-$PREV_TOTAL"
-  let "DIFF_USAGE=(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
+  DIFF_IDLE=$((IDLE-PREV_IDLE))
+  DIFF_TOTAL=$((TOTAL-PREV_TOTAL))
+  DIFF_USAGE=$(((1000*(DIFF_TOTAL-DIFF_IDLE)/DIFF_TOTAL+5)/10))
   echo -en "\rCPU: $DIFF_USAGE%  \b\b"
 
   # Remember the total and idle CPU times for the next check.
